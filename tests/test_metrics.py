381010bd999da358cda496dd97139f4ccced4eca
hydrogenhy <2237433588@qq.com>
2026-04-06 01:06:13 +0800

    delete test file

--- tests/test_metrics.py
+++
@@ -1,7 +0,0 @@
-from mdcompressor.metrics import measure_text
-
-
-def test_measure_text_counts_chars_and_bytes() -> None:
-    m = measure_text("abc你好")
-    assert m.chars == 5
-    assert m.bytes_utf8 == len("abc你好".encode("utf-8"))
